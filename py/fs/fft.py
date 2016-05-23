7fad484e5293f0e3af41627d1c1a58c74f21125b
Jun Koda <jun.koda@brera.inaf.it>
2016-05-24 06:18:55 +0900

    Add fft.py; Python FFT class.

---
+++ py/fs/fft.py
@@ -0,0 +1,16 @@
+import fs._fs as c
+
+class FFT:
+    def __init__(self, arg):
+        if isinstance(arg, int):
+            self._fft = c._fft_alloc(arg)
+        else:
+            self._fft = arg
+            # ToDo: Want to check more that arg is _FFT
+
+
+    def set_test_data(self):
+        c._fft_set_test_data(self._fft)
+
+    def asarray(self):
+        return c._fft_fx_global_as_array(self._fft)
