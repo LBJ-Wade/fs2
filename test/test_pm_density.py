c31e2ec6c817afc66935d3b57a0fd924013c2d9f
Jun Koda <jun.koda@brera.inaf.it>
2016-05-24 06:16:28 +0900

    Add parallel test.

--- test/test_pm_density.py
+++ test/test_pm_density.py
@@ -1,41 +1,42 @@
 #
 # Test PM density grid
 #
-import unittest
 import numpy as np
 import h5py
 import fs
 from create_pm_density import create_pm_density
 
+fs.set_loglevel(0)
 
-class TestFFT(unittest.TestCase):
-    def setUp(self):
-        filename = 'pm_density.h5'
-        file = h5py.File(filename, 'r')
-
-        self.delta_ref = file['delta']
-        file.close()
-
-        self.delta = create_pm_density()
-
-    def tearDown(self):
-        fs.comm_mpi_finalise()
-
-    def test_total(self):
-        delta = self.delta
-        nc = delta.shape
-        nmesh = nc[0]*nc[1]*nc[2]
-        eps = 1.0e-15
-
-        if fs.comm_this_node() == 0:
-            a = delta.astype(np.float64)
-
-            sum = np.sum(a)
-            print(sum)
-            self.assertTrue(sum < eps*nmesh)
-
-    # ToDo: compare delta and delta_ref
+delta = create_pm_density()
 
 
-if __name__ == '__main__':
-    unittest.main()
+#
+# Test total density is number of particles
+#
+
+if fs.comm_this_node() == 0:
+    nc = delta.shape
+    nmesh = nc[0]*nc[1]*nc[2]
+    eps = 1.0e-15
+
+    a = delta.astype(np.float64)
+
+    sum = np.sum(a)
+    print("Total %.3f" % sum)
+    assert(abs(sum) < eps*nmesh)
+
+#
+# Compare with serial mesh
+#
+    filename = 'pm_density.h5'
+    file = h5py.File(filename, 'r')
+    delta_ref = file['delta'][:]
+    file.close()
+
+    a = (delta - delta_ref).astype(np.float64)
+    diff = np.max(np.abs(a))
+    print('diff= %e' % diff)
+    assert(diff < 1.0e-7)
+
+fs.comm_mpi_finalise()
