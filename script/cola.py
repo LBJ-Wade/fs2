6e6fbfe486674bdad3d8214f82dd997b9bd0e458
Jun Koda <jun.koda@brera.inaf.it>
2016-08-03 12:49:07 +0200

    Fix cola.py for new module nameing convention.

--- script/cola.py
+++ script/cola.py
@@ -1,6 +1,21 @@
+#
+# Runs COmoving Lagrangian Acceleration simulation
+#
+# mpirun -n 1 python3 cola.py
+#
+# Input:
+#     Initial power spectrum: ../data/planck_matterpower.dat
+# Output:
+#     cola.h5
+#
+import signal
 import fs
 
 
+signal.signal(signal.SIGINT, signal.SIG_DFL) # enable cancel with ctrl-c
+
+
+# Parameters
 omega_m = 0.308
 nc = 64
 nc_pm = nc
@@ -10,24 +25,27 @@ a_final = 1.0
 seed = 1
 nstep = 9
 
-fs.cosmology_init(omega_m)
-ps = fs.PowerSpectrum('../../data/planck_matterpower.dat')
+# Initialisation
+fs.cosmology.init(omega_m)
+ps = fs.PowerSpectrum('../data/planck_matterpower.dat')
 
-particles = fs.lpt(nc, boxsize, a_init, ps, seed)
+# Initial condition
+particles = fs.lpt.init(nc, boxsize, a_init, ps, seed)
 
 fs.pm.init(nc_pm, nc_pm/nc, boxsize)
 
 for i in range(nstep):
     a_vel = a_init + (a_final - a_init)/nstep*(i + 0.5)
-    fs.pm.compute_force(particles)
-    fs.cola_kick(particles, a_vel)
+    fs.pm.force(particles)
+
+    fs.cola.kick(particles, a_vel)
 
     a_pos = a_init + (a_final - a_init)/nstep*(i + 1.0)
-    fs.cola_drift(particles, a_pos)
+    fs.cola.drift(particles, a_pos)
 
 filename = 'cola.h5'
 
 particles.save_hdf5(filename, 'ixv')
 print('%s written' % filename)
 
-fs.comm_mpi_finalise()
+
